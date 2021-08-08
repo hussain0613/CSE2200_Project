d6900fd5463879653f413d11b1d6f1ee765e1abe
hussain0613 <hussain0613@gmail.com>
2021-08-08 18:26:31 +0600

    added almost full settings read/write from/to  app storage, added reset defaults button, finished settings activity in back

--- app/src/main/java/com/example/cse2200_project/MainActivity.java
+++ app/src/main/java/com/example/cse2200_project/MainActivity.java
@@ -6,21 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView img_v;
     Button b;
     TextView tv;
+    Settings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settings = new Settings(this);
+
         b = findViewById(R.id.button);
         tv = findViewById(R.id.textView);
+
+        tv.setText("No Server is Running");
 
         b.setOnClickListener(v-> {
                 if(b.getText().equals("Start")){
--- app/src/main/java/com/example/cse2200_project/SettingsActivity.java
+++ app/src/main/java/com/example/cse2200_project/SettingsActivity.java
@@ -12,12 +12,15 @@ import android.widget.ToggleButton;
 public class SettingsActivity extends AppCompatActivity {
     EditText shared_dir_et, host_et, port_et;
     ToggleButton sub_dir_tb, upload_tb, mod_tb;
-    Button save_btn;
+    Button save_btn, reset_default_btn;
+
+    Settings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        settings = new Settings(this);
 
         shared_dir_et = findViewById(R.id.shared_directory_field);
         host_et = findViewById(R.id.host_field);
@@ -27,24 +30,38 @@ public class SettingsActivity extends AppCompatActivity {
         upload_tb = findViewById(R.id.upload_permission_field);
         mod_tb = findViewById(R.id.modify_permission_field);
 
+        set_from_file();
+
         save_btn = findViewById(R.id.save_btn);
+        reset_default_btn = findViewById(R.id.reset_default_btn);
 
         findViewById(R.id.back_img).setOnClickListener(e->go_back());
         findViewById(R.id.edit_img).setOnClickListener(e->toggle_edit());
 
         save_btn.setOnClickListener(e->save());
+        reset_default_btn.setOnClickListener(e -> reset_default());
     }
 
     void go_back(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
+
     void toggle_edit(){
         set_enable_all(!shared_dir_et.isEnabled());
     }
-    void save(){
 
+    void save(){
+        set_to_file();
+        set_enable_all(false);
     }
+
+    void reset_default(){
+        settings.setDefault();
+        set_from_file();
+    }
+
     void set_enable_all(boolean flag){
         shared_dir_et.setEnabled(flag);
         host_et.setEnabled(flag);
@@ -54,7 +71,33 @@ public class SettingsActivity extends AppCompatActivity {
         upload_tb.setEnabled(flag);
         mod_tb.setEnabled(flag);
 
-        if(flag)save_btn.setVisibility(View.VISIBLE);
-        else save_btn.setVisibility(View.GONE);
+        if(flag){
+            save_btn.setVisibility(View.VISIBLE);
+            reset_default_btn.setVisibility(View.VISIBLE);
+        }
+        else {
+            save_btn.setVisibility(View.GONE);
+            reset_default_btn.setVisibility(View.GONE);
+        }
+    }
+
+    void set_from_file(){
+        shared_dir_et.setText((String)settings.get("shared_directory"));
+        host_et.setText((String) settings.get("host"));
+        port_et.setText(String.valueOf(settings.get("port")));
+
+        sub_dir_tb.setChecked((boolean) settings.get("share_sub_directory"));
+        upload_tb.setChecked((boolean) settings.get("upload_files_permission"));
+        mod_tb.setChecked((boolean) settings.get("modify_directories_permission"));
+    }
+    void set_to_file(){
+        settings.set("shared_directory", shared_dir_et.getText().toString());
+        settings.set("host", host_et.getText().toString());
+        settings.set("port", Integer.parseInt(port_et.getText().toString()));
+
+        settings.set("share_sub_directory", sub_dir_tb.isChecked());
+        settings.set("upload_files_permission", upload_tb.isChecked());
+        settings.set("modify_directories_permission", mod_tb.isChecked());
+        settings.write_settings();
     }
 }
